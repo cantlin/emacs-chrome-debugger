e9ea6bc5cc67bf574f9a95e37ce758f544d79137
Cantlin Ashrowan <cantlin@ashrowan.com>
2011-10-19 12:29:32 +0100

    Working copy

---
+++ README.md
@@ -0,0 +1,5 @@
+### emacs-chrome-debugger
+
+Fork of [langmartin/emacs-chrome-debugger](https://github.com/langmartin/emacs-chrome-debugger/). Implements [ChromeDevToolsProtocol](http://code.google.com/p/chromedevtools/wiki/ChromeDevToolsProtocol][ChromeDevToolsProtocol). Will update to use the Remote Debugging Protocol when it becomes clear how to do that.
+
+**Disclaimer:** I do not know Lisp. I just hacked it until it worked.
--- chrome-debugger.el
+++ chrome-debugger.el
@@ -1,69 +1,81 @@
-;;;; Interface functions are at the bottom.
+(require 'json)
 
 (defcustom chrome-debugger-host "localhost"
-  "Hostname of the server running the ChromeDevTools"
+  "Hostname of the server running ChromeDevTools."
   :group 'chrome-debugger
   :type 'string)
 
 (defcustom chrome-debugger-port 9222
-  "Port for the server running the ChromeDevTools"
+  "Port for the server running ChromeDevTools."
   :group 'chrome-debugger
   :type 'integer)
 
-(defvar chrome-debugger-buffer "*chrome debugger*"
-  "Name of the chrome-debugger buffer")
+(defvar chrome-debugger-buffer-name "*chrome-debugger*"
+  "Name of the chrome-debugger buffer.")
 
-(defvar chrome-debugger-stream "*chrome debugger*"
-  "Name of the chrome-debugger network stream")
+(defvar chrome-debugger-stream-name "chrome-debugger"
+  "Name of the chrome-debugger network stream.")
 
-(defmacro with-chrome-debugger-repl (body)
+(defmacro in-chrome-debugger-buffer (body)
   `(save-excursion
-     (let ((chrome-debugger-buffer (get-buffer chrome-debugger-buffer)))
-       (if (null chrome-debugger-buffer)
-           (open-network-stream
-            chrome-debugger-stream
-            chrome-debugger-buffer
-            chrome-debugger-host
-            chrome-debugger-port))
-       (set-buffer chrome-debugger-buffer)
-       ,@body)))
+     (set-buffer chrome-debugger-buffer)
+     ,body))
 
-(defun chrome-debugger-send* (str)
-  "Send a literal string to chrome-debugger-process."
-  (process-send-string chrome-debugger-stream str))
+(defun chrome-debugger-send (process str)
+  "Send string to process and insert it into the current buffer."
+  (process-send-string process str)
+  (insert (format "-- Sent --\n%s\n" str)))
 
-(defun chrome-debugger-send (cmd tool &optional dest)
-  "Send a command assuming the environment is set correctly."
-  (chrome-debugger-send* "Content-Length: ")
-  (chrome-debugger-send* (+ (length cmd) 2))
-  (chrome-debugger-send* crlf)
-  (chrome-debugger-send* "Tool: ")
-  (chrome-debugger-send* tool)
-  (chrome-debugger-send* crlf)
-  (if (dest)
-      (progn
-        (chrome-debugger-send* "Destination: ")
-        (chrome-debugger-send* dest)
-        (chrome-debugger-send* crlf)))
-  (chrome-debugger-send* crlf)
-  (chrome-debugger-send* cmd))
+(defun chrome-debugger-read (process str)
+  "Receive string from process and insert it into the chrome-debugger buffer."
+  (if (not (eq str "ChromeDevToolsHandshake\r\n"))
+    (progn
+      (setq chrome-debugger-last-response (json-read-from-string (car (last (split-string str "\n\n")))))
+      (in-chrome-debugger-buffer
+        (insert (format "-- Received --\n%s\n" chrome-debugger-last-response))))))
 
-(defun chrome-debugger-read* ()
-  "Read a response assuming the environment is set correctly."
-  
-  )
+(defun chrome-debugger-formatted-request (command tool &optional destination)
+  "Send a formatted request to the ChromeDevTools server."
+  (chrome-debugger-send chrome-debugger-stream
+    (format (concat "Content-Length:%d\r\n"
+                    "Tool:%s\r\n"
+                    "Destination:%s\r\n"
+                    "\r\n"
+                    "%s")
+            (length command) tool (if (boundp 'destination) destination "") command)))
 
-(defun chrome-debugger-handshake ()
+(defun chrome-debugger-devtools-command (command)
+  "Send an arbitrary command to the DevToolsService.
+   Available commands: `ping`, `version`, `list_tabs`."
+  (interactive "sCommand: ")
+  (in-chrome-debugger-buffer
+    (chrome-debugger-formatted-request (format "{\"command\":\"%s\"}" command) "DevToolsService")))
+
+(defun chrome-debugger-v8-command (command destination)
+  "Send an arbitrary command to the V8Debugger."
+  (interactive "sCommand: \nsDestination: ")
+  (in-chrome-debugger-buffer
+    (chrome-debugger-formatted-request command "V8Debugger" destination)))
+
+(defun chrome-debugger-reload-first-tab () ""
   (interactive)
-  (with-chrome-debugger-repl
-   (let ((text "ChromeDevToolsHandshake"))
-    (chrome-debugger-echo* text)
-    (if (not (equal text (chrome-debugger-read*)))
-        (error "handshake with the ChromeDevTools server failed.")
-      t))))
-
-(defun chrome-debugger-send (cmd)
-  "Send a command, setting the environment"
-  (interactive "sSend: ")
-  (with-chrome-debugger-repl
-   (chrome-debugger-send* cmd)))
+  (chrome-debugger-devtools-command 'list_tabs)
+  (accept-process-output chrome-debugger-stream)
+  (chrome-debugger-v8-command
+    (json-encode '((command . "evaluate_javascript") (data . "window.location.reload();")))
+    (aref (aref (cdr (assoc 'data chrome-debugger-last-response )) 0) 0)))
+ 
+(setq chrome-debugger-buffer (get-buffer-create chrome-debugger-buffer-name))
+(if (not (boundp 'chrome-debugger-stream))
+  (progn
+    (setq chrome-debugger-stream (open-network-stream
+                                  chrome-debugger-stream-name
+                                  chrome-debugger-buffer
+                                  chrome-debugger-host
+                                  chrome-debugger-port))
+    (set-process-filter-multibyte chrome-debugger-stream t)
+    (set-process-coding-system chrome-debugger-stream 'utf-8 'utf-8)
+    (set-process-filter chrome-debugger-stream 'chrome-debugger-read)
+    (setq chrome-debugger-last-response nil)
+    (in-chrome-debugger-buffer
+      (chrome-debugger-send chrome-debugger-stream "ChromeDevToolsHandshake\r\n"))))
